30355a8801e74bfa11b81c5eb70268c68b89657e
KibetBrian <briankibet2040@gmail.com>
2022-06-29 22:01:17 +0300

    update routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import WatchList from './sections/WatchList'
 import News from './sections/News'
 import LeftBar from './sections/LeftBar'
 import { useSelector } from 'react-redux'
+import VoterRegistration from './sections/VoterRegistration'
+import ContestantRegistration from './sections/ContestantRegistration'
+import SystemInformation from './sections/SystemInformation'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   const user = useSelector((state) => state.user.user.payload)
@@ -20,11 +24,15 @@ const App = () => {
 
         <Route path='/'>
           <Route index element={user ? <Home /> : <Navigate to="/auth/login" />} />
-          <Route path={"/auth/login"} element={user ? <Navigate to="/" /> : <Login />} />
+          <Route path={"/auth/"}>
+            <Route path={"/auth/login"} element={user ? <Navigate to="/" /> : <Login />} />
+            <Route path={"/auth/signup"} element={user ? <Navigate to="/" /> : <SignUp />} />
+          </Route>
           <Route path={"news"} element={<News />} />
+          <Route path={"/voter/registration"} element={<VoterRegistration />} />
           <Route path="contestants" element={<Contestants />} />
-          <Route path="watchlist" element={<WatchList />} />
-
+          <Route path={"/contestant/registration"} element={<ContestantRegistration />} />
+          <Route path={"/information"} element={<SystemInformation />} />
         </Route>
 
       </Routes>
