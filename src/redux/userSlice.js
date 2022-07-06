41f2e42da3d82af469e51db8afd140ccd2419693
KibetBrian <briankibet2010@gmail.com>
2022-07-06 17:24:00 +0300

    update

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -2,25 +2,25 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 const initialState = {
-    userData: null,
-    isFetching: false,
-    error: false
+  data: null,
+  isFetching: false,
+  error: false
 }
 
 export const userSlice = createSlice(
-  { 
+  {
     name: 'user',
     initialState,
     reducers: {
-        isFetching: (state, data)=>{
-          state.isFetching = data.payload
-        },
-        setUserData: (state, data)=>{
-          state.user=data
-        }
+      isFetching:(state, data) => {
+        state.isFetching=data.payload
+      },
+      setUserData: (state, data) => {
+        state.data=data.payload
+      }
     }
   }
 );
 
-export const {setUserData, isFetching} =userSlice.actions;
+export const { setUserData, isFetching } = userSlice.actions;
 export default userSlice.reducer;
