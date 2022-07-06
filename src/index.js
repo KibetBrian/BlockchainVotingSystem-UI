749c89facbfe9850ed0ace532597afb231eec65d
KibetBrian <briankibet2010@gmail.com>
2022-07-06 17:26:57 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { theme } from './theme';
 import App from './App';
-import { store } from './redux/store'
+import  store from './redux/store'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
