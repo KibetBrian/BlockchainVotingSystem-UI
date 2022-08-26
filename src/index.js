16db94954507a136f28783530eeb338bf2e8d6d3
KibetBrian <briankibet2040@gmail.com>
2022-08-26 20:42:59 +0300

    adds redux persist

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { theme } from './theme';
 import App from './App';
-import  store from './redux/store'
+import store from './redux/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
