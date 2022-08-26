d7d6c38d13e7d9d8c70518ea7899d5a77ae15143
KibetBrian <briankibet2040@gmail.com>
2022-08-26 20:40:51 +0300

    persists store to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSliceReducer from './userSlice'
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+  key: 'counter',
+  storage,
+};
+const reducers = combineReducers({ user: userSliceReducer });
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: {
-    user: userSliceReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+          serializableCheck: {
+              ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+      }),
+});
