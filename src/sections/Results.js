74437be114bb70901a94718d9af4e8be1d5ce0f7
KibetBrian <briankibet2040@gmail.com>
2022-08-26 20:38:44 +0300

    handle search

--- src/sections/Results.js
+++ src/sections/Results.js
@@ -18,6 +18,10 @@ const SearchBar = (props) => {
   const handleClick = () => {
     props.changeState(false)
   }
+  const handleFilter = (e)=>{
+    props.setSearchInput(e.target.value)
+  }
+
   return (
     <>
       <Box sx={{ zIndex: 1, height: '40px', left: '30%', position: 'absolute', backdropFilter: 'blur(6px)', width: '50%', display: 'flex', padding: 2, justifyContent: 'space-between', alignItems: "center", borderRadius: theme.border.regular, boxShadow: "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)" }}>
@@ -26,6 +30,7 @@ const SearchBar = (props) => {
           sx={{ ml: 3, flex: 1, }}
           placeholder="Search...."
           inputProps={{ 'aria-label': 'search...' }}
+          onChange = {handleFilter}
         />
         <Button onClick={handleClick} sx={{ ":hover": { backgroundColor: theme.palette.primary.darkGreen }, backgroundColor: theme.palette.primary.main, color: theme.palette.primary.white }}>
           Search
@@ -35,7 +40,7 @@ const SearchBar = (props) => {
   )
 }
 
-const TopBar = () => {
+const TopBar = (props) => {
   const [notMounted, setNotMounted] = useState(true);
   const imageAddress = "https://images.pexels.com/photos/5358099/pexels-photo-5358099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
   const countryAddress = "https://countryflagsapi.com/png/ke"
@@ -62,11 +67,15 @@ const TopBar = () => {
     dispatch(setUserData(null))
   }
 
+  const handleClickAway = () => {
+    setSearchBarOpen(false)
+  }
+
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
 
   return (
-    <ClickAwayListener onClickAway={() => setSearchBarOpen(false)}>
+    <ClickAwayListener onClickAway={handleClickAway}>
       <Box sx={{ height: 70, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <IconButton sx={searchBarOpen ? { opacity: '0' } : { opacity: '1' }} onClick={() => setSearchBarOpen(true)}>
           <BsSearch />
@@ -74,7 +83,7 @@ const TopBar = () => {
         <Slide direction='down' in={searchBarOpen}>
           <Box sx={{ width: '50vw' }}>
             <Box sx={{ display: 'flex', width: '100%' }}>
-              {searchBarOpen && <SearchBar changeState={setSearchBarOpen} />}
+              {searchBarOpen && <SearchBar setSearchInput={props.setSearchInput} data={props} changeState={setSearchBarOpen} />}
             </Box>
           </Box>
         </Slide>
@@ -92,8 +101,9 @@ const CardComponent = ({ data }) => (
     <Card sx={{ maxWidth: 345, mt: 2 }}>
       <CardMedia
         component="img"
-        height="140"
+        height="200"
         image={data.imageAddress}
+        sx={{objectFit: 'inherit'}}
         alt="green iguana"
       />
       <CardContent>
@@ -114,30 +124,30 @@ const CardComponent = ({ data }) => (
 
 const Results = () => {
   const [data, setData] = useState([]);
-  console.log(data)
+  const [searchInput, setSearchInput] = useState("");
+  
   const dispatch = useDispatch();
 
   const user = useSelector(state => state.user);
-
-  useEffect(() => {
-    const FetchData = async () => {
-      try {
-        dispatch(isFetching(true))
-        const response = await client.get("/results/presidential");
-        setData(response.data);
-        dispatch(isFetching(false))
-      } catch (e) {
-        console.log(e);
-        dispatch(isFetching(false))
-      }
+  const FetchData = async () => {
+    try {
+      dispatch(isFetching(true))
+      const response = await client.get("/results/presidential");
+      setData(response.data);
+      dispatch(isFetching(false))
+    } catch (e) {
+      console.log(e);
+      dispatch(isFetching(false))
     }
+  }
+  useEffect(() => {
     FetchData();
   }, [])
   return (
     <Box sx={{ display: 'flex' }}>
       <LeftBar />
       <Box sx={{ flex: 4, display: 'flex', height: '100vh', flexFlow: 'row wrap', justifyContent: 'space-around', overflowY: 'scroll', }}>
-        <TopBar />
+        <TopBar setSearchInput={setSearchInput} data={data} fetchData={FetchData} setData={setData}/>
         <Backdrop
           sx={{ color: '#000', zIndex: 100 }}
           open={user.isFetching}
@@ -149,7 +159,7 @@ const Results = () => {
             </Box>
           </Box>
         </Backdrop>
-        {data.map((item, index) => (<CardComponent key={index} data={item} />))}
+        {data.filter(e=>e.fullName.toLowerCase().includes(searchInput.toLowerCase())).map((item, index) => (<CardComponent key={index} data={item} />))}
       </Box>
     </Box>
   )
