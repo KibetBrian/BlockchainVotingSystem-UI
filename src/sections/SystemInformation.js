183fa377b270da3b85747a53d176523dca348bf7
KibetBrian <briankibet2040@gmail.com>
2025-01-24 21:02:50 +0300

    refactor SystemInformation component for improved readability and structure

--- src/sections/SystemInformation.js
+++ src/sections/SystemInformation.js
@@ -1,65 +1,84 @@
-import { ListItemText, Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
-import LeftBar from './LeftBar'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import LeftBar from './LeftBar';
 
 const SystemInformation = () => {
+    const renderUserTypeSection = (title, details) => (
+        <>
+            <li>{title}</li>
+            <Box sx={{ ml: 5 }}>
+                {details.map((detail, index) => (
+                    <li key={index}>{detail}</li>
+                ))}
+            </Box>
+        </>
+    );
+
+    const renderElectionPhaseSection = (title, description) => (
+        <>
+            <li>{title}</li>
+            <Box sx={{ ml: 5 }}>
+                <li>{description}</li>
+            </Box>
+        </>
+    );
+
     return (
         <Box sx={{ display: 'flex' }}>
             <LeftBar />
-            <Box sx={{ flex: 4 }}>
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Box sx={{ flex: 4, p: 2 }}>
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 3 }}>
                     <Typography component="h5" variant="h5">
                         Blockchain Voting System
                     </Typography>
                 </Box>
-                <Typography sx={{mt:2,mb:2}} component="h5" variant="h5">System Users</Typography>
-                <Typography component="p" variant="p">
+
+                <Typography sx={{ mt: 3, mb: 1.5 }} component="h5" variant="h5">
+                    System Users
+                </Typography>
+                <Typography component="p" variant="body1" sx={{ mb: 2 }}>
                     The system will be composed of three types of users which include:
                 </Typography>
                 <Box sx={{ ml: 5 }}>
                     <ul>
-                        <li>Normal Users</li>
-                        <Box sx= {{ml: 5}}>
-                            <li> They can be able to sign in and check the information on the system about candidates and election results</li>
-                            <li>If they have valid credentials depending on the organization data, they can request to be registered as a voter by submitting their credentials</li>
-                        </Box>
-                        <li>Registered Voters</li>
-                        <Box sx={{ml: 5}}>
-                            <li>Registered voters need to have metamask extension in order to interact with the smart contracts on the blockchain</li>
-                            <li>They will have ability to choose their desired leader by casting their votes</li>
-                        </Box>
-                        <li>Admin</li>
-                        <Box sx={{ml: 5}}>
-                            <li>Admin will have the ability to register new candidates and voters. This is by crosschecking the voters and and candidates submitted data and the organization data if they are valid then their addresses will be added to valid voters on the blockchain</li>
-                        </Box>
+                        {renderUserTypeSection('Normal Users', [
+                            'Can sign in and check information on the system about candidates and election results',
+                            'Can request to be registered as a voter by submitting valid credentials'
+                        ])}
+                        {renderUserTypeSection('Registered Voters', [
+                            'Need to have MetaMask extension to interact with smart contracts on the blockchain',
+                            'Can choose their desired leader by casting votes'
+                        ])}
+                        {renderUserTypeSection('Admin', [
+                            'Can register new candidates and voters',
+                            'Crosscheck voters and candidates submitted data against organization data',
+                            'Add valid addresses to voters on the blockchain'
+                        ])}
                     </ul>
                 </Box>
-                <Typography  sx={{mt:2,mb:2}} component="h5" variant="h5">Election Information</Typography>
-                <Typography component ="p" variant = "p">The election will have three phases</Typography>
+
+                <Typography sx={{ mt: 3, mb: 1.5 }} component="h5" variant="h5">
+                    Election Information
+                </Typography>
+                <Typography component="p" variant="body1" sx={{ mb: 2 }}>
+                    The election will have three phases:
+                </Typography>
                 <Box sx={{ ml: 5 }}>
                     <ul>
-                        <li>Registration Phase</li>
-                        <Box sx= {{ml: 5}}>
-                            <li> This is where potential voters will submit their data for verification if they are eligible to be voter for the particular organization</li>
-                        </Box>
-                        <li>Voting Phase</li>
-                        <Box sx={{ml: 5}}>
-                            <li>This is where verified voters will participate in casting their voters</li>
-                        </Box>
-                        <li>Results phase</li>
-                        <Box sx={{ml: 5}}>
-                            <li>After election is over, everyone can now access election results</li>
-                        </Box>
+                        {renderElectionPhaseSection('Registration Phase', 
+                            'Potential voters submit data for verification of eligibility'
+                        )}
+                        {renderElectionPhaseSection('Voting Phase', 
+                            'Verified voters participate in casting their votes'
+                        )}
+                        {renderElectionPhaseSection('Results Phase', 
+                            'Election results are accessible to everyone after the election concludes'
+                        )}
                     </ul>
                 </Box>
-                <Box>
-
-                </Box>
-
             </Box>
         </Box>
-    )
-}
+    );
+};
 
-export default SystemInformation
+export default SystemInformation;
